6a370f9b10a483ded56ab04f3ae2c8db0fc49256
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2021-10-04 13:18:00 -0300

    Create port-scanner.py

---
+++ port-scanner.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+
+NombreHost= socket.gethostname()
+DireccionIp= socket.gethostbyname(NombreHost)
+
+objetivo=socket.gethostbyname(DireccionIp)
+
+print("Escaneando...")
+
+try:
+  for port in range(1,150):
+      s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+      socket.setdefaulttimeout(1)
+      resultado=s.connect_ex((objetivo,port))
+      print(".")
+      if resultado==0:
+         print("El puerto {} está abierto".format(port))
+      s.close()
+except:
+  print("\n Terminando programa...")
+  sys.exit(0)

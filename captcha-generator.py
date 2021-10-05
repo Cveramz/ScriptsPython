737e9e0b95cb455fb2006befa5e4eec7330f77d1
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2021-10-05 13:11:08 -0300

    Create captcha-generator.py

---
+++ captcha-generator.py
@@ -0,0 +1,12 @@
+from captcha.image import ImageCaptcha
+from random import randrange
+
+image=ImageCaptcha(width=280, height=90)
+
+captcha_text=str(randrange(9999,99999))
+
+data=image.generate(captcha_text)
+
+image.write(captcha_text,"Captcha.png")
+
+print("Captcha generado correctamente")
